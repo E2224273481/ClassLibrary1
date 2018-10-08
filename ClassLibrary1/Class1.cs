959e2649d445bc40946229dece31e191db1a0562
1281945615 <1281945615@qq.com>
2018-10-09 00:09:18 +0800

    第一次修改

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Class1
+    public class Class1 
     {
+        /// <summary>
+        /// fsdfds
+        /// </summary>
+        int b = 10;
+        int c = 20;
+        int a;
+        private void Addaaa()
+        {
+            a = b + c;
+            Console.Write(a);
+        }
     }
 }
